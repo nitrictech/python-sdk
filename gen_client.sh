f75d5163ac58178926a30812dc0a50b7a262dba3
Jye Cusch <jye.cusch@gmail.com>
2020-11-03 17:04:22 +1100

    chore: add grpc client generation

---
+++ gen_client.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# python3 -m grpc_tools.protoc --js_out=import_style=commonjs,binary:./src/interfaces --grpc_out=./src/interfaces --plugin=protoc-gen-v1=`which grpc_tools_node_protoc_plugin` -I ./contracts ./contracts/**/*.proto
+CONTRACTS=./contracts/proto
+OUTPUT=./nitric/proto
+mkdir -p ${OUTPUT}
+python3 -m grpc_tools.protoc -I ${CONTRACTS}  --python_out=${OUTPUT} --grpc_python_out=${OUTPUT} ./contracts/proto/**/*.proto
