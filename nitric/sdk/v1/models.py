2ad0ca8320f614b8c9d3e2440053845a7fd45078
Jye Cusch <jye.cusch@nitric.io>
2021-02-02 14:02:46 +1100

    feat(queue): add queue pop method
    Also, cleaned up private method names in the queue client

--- nitric/sdk/v1/models.py
+++ nitric/sdk/v1/models.py
@@ -23,3 +23,12 @@ class FailedEvent(object):
         """Construct a new Failed Event."""
         self.event = event
         self.message = message
+
+
+class QueueItem(object):
+    """Represents a NitricEvents from a Queue that must be acknowledged once complete."""
+
+    def __init__(self, event: Event, lease_id: str):
+        """Constructs a new Queue Item, containing an Event."""
+        self.event = event
+        self.lease_id = lease_id
--- nitric/sdk/v1/queue_client.py
+++ nitric/sdk/v1/queue_client.py
@@ -4,7 +4,7 @@ from nitric.proto import queue_service
 from nitric.proto import common
 from nitric.sdk.v1._base_client import BaseClient
 from google.protobuf.struct_pb2 import Struct
-from nitric.sdk.v1.models import Event, FailedEvent
+from nitric.sdk.v1.models import Event, FailedEvent, QueueItem
 from google.protobuf.json_format import MessageToDict
 
 
@@ -28,7 +28,7 @@ class QueueClient(BaseClient):
         super(self.__class__, self).__init__()
         self._stub = queue_service.QueueStub(self._channel)
 
-    def evt_to_wire(self, event: Event) -> common.NitricEvent:
+    def _evt_to_wire(self, event: Event) -> common.NitricEvent:
         """
         Convert a Nitric Event to a Nitric Queue Event.
 
@@ -44,23 +44,40 @@ class QueueClient(BaseClient):
             payload=payload_struct,
         )
 
-    def wire_to_failed_evt(self, event: queue.FailedMessage) -> FailedEvent:
+    def _wire_to_event(self, event: common.NitricEvent) -> Event:
+        """
+        Convert a Nitric Queue Event (protobuf) to a Nitric Event (python SDK)
+        :param event: to convert
+        :return: converted event
+        """
+
+        return Event(
+            request_id=event.requestId,
+            payload_type=event.payloadType,
+            payload=MessageToDict(event.payload)
+        )
+
+    def _wire_to_queue_item(self, item: queue.NitricQueueItem) -> QueueItem:
+        """
+        Convert a NitricQueueItem to the Python SDK model equivalent QueueItem
+
+        :param item: to be converted
+        :return: the converted queue item, containing the associated event
+        """
+        evt = self._wire_to_event(item.event)
+
+        return QueueItem(event=evt, lease_id=item.leaseId)
+
+    def _wire_to_failed_evt(self, failed_message: queue.FailedMessage) -> FailedEvent:
         """
         Convert a queue event that failed to push into a Failed Event object.
 
-        :param event: the failed event
+        :param failed_message: the failed event
         :return: the Failed Event with failure message
         """
-        tmp_evt = event.event
-        tmp_msg = event.message
+        evt = self._wire_to_event(failed_message.event)
 
-        evt = Event(
-            request_id=tmp_evt.request_id,
-            payload_type=tmp_evt.payload_type,
-            payload=MessageToDict(tmp_evt.payload),
-        )
-
-        return FailedEvent(event=evt, message=tmp_msg)
+        return FailedEvent(event=evt, message=failed_message.message)
 
     def push(self, queue_name: str, events: List[Event] = None) -> PushResponse:
         """
@@ -72,12 +89,32 @@ class QueueClient(BaseClient):
         """
         if events is None:
             events = []
-        wire_events = map(self.evt_to_wire, events)
+        wire_events = map(self._evt_to_wire, events)
 
         request = queue.PushRequest(queue=queue_name, events=wire_events)
 
         response: queue.PushResponse = self._exec("Push", request)
 
-        failed_events = map(self.wire_to_failed_evt, response.failedMessages)
+        failed_events = map(self._wire_to_failed_evt, response.failedMessages)
 
         return PushResponse(failed_events=list(failed_events))
+
+    def pop(self, queue_name: str, depth: int = None) -> List[QueueItem]:
+        """
+        Pops 1 or more items from the specified queue up to the depth limit.
+
+        :param queue_name: Nitric name for the queue. This will be automatically resolved to the provider specific identifier.
+        :param depth: The maximum number of queue items to return. Default: 1, Min: 1.
+        :return: Queue items popped from the specified queue.
+        """
+
+        # Set the default and minimum depth to 1.
+        if depth is None or depth < 1:
+            depth = 1
+
+        request = queue.PopRequest(queue=queue_name, depth=depth)
+
+        response: queue.PopResponse = self._exec("Pop", request)
+
+        # Map the response protobuf response items to Python SDK Nitric Queue Items
+        return [self._wire_to_queue_item(item) for item in response.items]
