efd84cb0a84d27d6b217b9cec813e9ef7cf519ba
Jye Cusch <jye.cusch@nitric.io>
2021-02-24 13:57:00 +1100

    fix: use correct service host env var names

--- nitric/config/default_settings.py
+++ nitric/config/default_settings.py
@@ -1,4 +1,4 @@
 # Provides a set of default settings that env vars can replace
 
-AMBASSADOR_ADDRESS = "127.0.0.1"
-AMBASSADOR_PORT = "50051"
+NITRIC_SERVICE_HOST = "127.0.0.1"
+NITRIC_SERVICE_PORT = "50051"
