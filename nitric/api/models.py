d9eebc2385174608c7dfc8fa0ba21ddc7633ad35
Jye Cusch <jye.cusch@nitric.io>
2021-05-08 19:51:46 +1000

    feat: switch models to dataclasses

--- nitric/api/models.py
+++ nitric/api/models.py
@@ -16,41 +16,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from dataclasses import dataclass, field
 
 
+@dataclass(frozen=True, order=True)
 class Topic(object):
     """Represents event topic metadata."""
 
-    def __init__(self, name: str):
-        """Construct a new topic instance."""
-        self.name = name
+    name: str
 
 
+@dataclass(frozen=True, order=True)
 class Event(object):
     """Represents a NitricEvent."""
 
-    def __init__(self, event_id: str, payload_type: str, payload: dict):
-        """Construct a new Event."""
-        self.event_id = event_id
-        self.payload_type = payload_type
-        self.payload = payload
+    event_id: str
+    payload_type: str
+    payload: dict
 
 
+@dataclass(frozen=True, order=True)
 class Task(object):
     """Represents a NitricTask."""
 
-    def __init__(self, task_id: str, payload_type: str, payload: dict, lease_id: str = None):
-        """Construct a new Task."""
-        self.task_id = task_id
-        self.payload_type = payload_type
-        self.payload = payload
-        self.lease_id = lease_id
+    task_id: str
+    payload_type: str
+    payload: dict
+    lease_id: str = field(default=None)
 
 
+@dataclass(frozen=True, order=True)
 class FailedTask(Task):
     """Represents a failed queue publish for an event."""
 
-    def __init__(self, task: Task, message: str):
-        """Construct a new Failed Event."""
-        super().__init__(task.task_id, task.payload_type, task.payload, task.lease_id)
-        self.message = message
+    message: str = field(default="")
