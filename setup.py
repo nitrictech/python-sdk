24c030c0d85296a636f5deba81b8e3cc3171b13b
Jye Cusch <jye.cusch@nitric.io>
2021-06-25 14:47:06 +1000

    chore: move git tag code into setup

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
 import setuptools
-from tools.git_tag import get_current_version_tag
+import re
+from subprocess import Popen, PIPE
+
+
+def get_current_version_tag():
+    process = Popen(["git", "describe", "--tags", "--match", "v[0-9]*"], stdout=PIPE)
+    (output, err) = process.communicate()
+    process.wait()
+
+    tags = str(output, "utf-8").strip().split("\n")
+
+    version_tags = [tag for tag in tags if re.match(r"^v?(\d*\.){2}\d$", tag)]
+    dev_tags = [tag for tag in tags if re.match(r"^v?(\d*\.){2}\d-\d*-[a-z\d]{8}$", tag)]
+
+    if len(version_tags) == 1:
+        return version_tags.pop()[1:]
+    elif len(dev_tags) == 1:
+        base_tag, num_commits = dev_tags.pop().split("-")[:2]
+        return "{}.dev{}".format(base_tag, num_commits)[1:]
+    else:
+        raise Exception("Unable to determine or construct version from git tags.")
+
 
 with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
@@ -13,7 +34,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nitrictech/python-sdk",
-    packages=setuptools.find_packages(".", exclude=["tests", "tools"]),
+    packages=setuptools.find_packages(),
     license_files=("LICENSE.txt",),
     classifiers=[
         "Programming Language :: Python :: 3",
