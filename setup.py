f7f7195468a5b241743996a368a794638fb49bbc
Jye Cusch <jye.cusch@nitric.io>
2021-06-25 14:55:40 +1000

    chore: don't distribute tests packages

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/nitrictech/python-sdk",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
     license_files=("LICENSE.txt",),
     classifiers=[
         "Programming Language :: Python :: 3",
