c483ca62612184faddf4ad3b5cbe5269676ae99a
Ryan Cartwright <ryan.cartwright@nitric.io>
2021-08-16 11:19:40 +1000

    test: Add tests to snippets

--- examples/documents/refs.py
+++ examples/documents/refs.py
@@ -1,12 +1,13 @@
 # [START import]
 from nitric.api import Documents
 # [END import]
+def documents_refs():
 # [START snippet]
-docs = Documents()
+  docs = Documents()
 
-# create a reference to a collection named 'products'
-products = docs.collection("products")
+  # create a reference to a collection named 'products'
+  products = docs.collection("products")
 
-# create a reference to a document with the id 'nitric'
-nitric = products.doc("nitric")
+  # create a reference to a document with the id 'nitric'
+  nitric = products.doc("nitric")
 # [END snippet]
