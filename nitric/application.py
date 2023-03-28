c90631c11b83d425413a92e2bcc5dd3e9ab1f796
Ryan Cartwright <fryzee.cartwright@gmail.com>
2023-03-29 09:09:03 +1100

    add support for open telemetry

--- nitric/application.py
+++ nitric/application.py
@@ -1,4 +1,30 @@
+#
+# Copyright (c) 2021 Nitric Technologies Pty Ltd.
+#
+# This file is part of Nitric Python 3 SDK.
+# See https://github.com/nitrictech/python-sdk for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 import asyncio
+import os
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider, sampling
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.grpc import GrpcInstrumentorClient
+
 from nitric.faas import FunctionServer
 from nitric.api.exception import NitricUnavailableException
 
@@ -41,12 +67,34 @@ class Nitric:
             )
 
     @classmethod
+    def _create_tracer(cls) -> TracerProvider:
+        local_run = os.environ.OTELCOL_BIN is not None
+        samplePercent = os.environ.NITRIC_TRACE_SAMPLE_PERCENT
+
+        # If its a local run use a console exporter, otherwise export using OTEL Protocol
+        exporter = OTLPSpanExporter(endpoint="http://localhost:4317", insecure=True)
+        if local_run:
+            exporter = ConsoleSpanExporter()
+
+        provider = TracerProvider(
+            active_span_processor=BatchSpanProcessor(exporter),
+            sampler=sampling.TraceIdRatioBased(samplePercent / 100 if samplePercent is not None else 100),
+        )
+        trace.set_tracer_provider(provider)
+
+        grpc_client_instrumentor = GrpcInstrumentorClient()
+        grpc_client_instrumentor.instrument()
+
+        return provider
+
+    @classmethod
     def run(cls):
         """
         Start the nitric application.
 
         This will execute in an existing event loop if there is one, otherwise it will attempt to create its own.
         """
+        provider = cls._create_tracer()
         try:
             try:
                 loop = asyncio.get_running_loop()
@@ -60,3 +108,6 @@ class Nitric:
             raise NitricUnavailableException(
                 'Unable to connect to a nitric server! If you\'re running locally make sure to run "nitric start"'
             )
+        finally:
+            if provider is not None:
+                provider.force_flush()
